f6a867d2066f43155081d46640aa429e7b8a91f2
Olaf Merkert <olaf@m-merkert.de>
2013-06-26 17:59:15 +0200

    provide stop-server function

--- hunchentoot-helpers.lisp
+++ hunchentoot-helpers.lisp
@@ -1,8 +1,16 @@
 (in-package :web-utils)
 
+(defparameter *current-web-server* nil)
+
 (defun start-server (&optional production)
-  (hunchentoot:start
-   (make-instance 'hunchentoot:easy-acceptor :port (if production 80 8080))))
+  (unless *current-web-server*
+   (hunchentoot:start
+    (setf *current-web-server*
+          (make-instance 'hunchentoot:easy-acceptor
+                         :port (if production 80 8080))))))
+
+(defun stop-server ()
+  (hunchentoot:stop *current-web-server*))
 
 (defun error-code (&optional (code hunchentoot:+HTTP-NOT-FOUND+))
   (setf (hunchentoot:return-code*) code)

5f9f3c9578fa58b8fc4e3e54db8deeddbced5bea
Olaf Merkert <olaf@m-merkert.de>
2014-08-09 23:01:25 +0200

    enhance navbar macro to handle empty list of links and additional navbar content

--- bootstrap-helpers.lisp
+++ bootstrap-helpers.lisp
@@ -4,7 +4,9 @@
   (:export
    #:navbar
    #:elink
-   #:link))
+   #:link
+   #:bs-body
+   #:navbar+))
 
 (in-package :bootstrap-helpers)
 
@@ -14,22 +16,34 @@
 (defmacro link (target label)
   `(htm (:a :href ,target ,label)))
 
-(defmacro navbar (title &rest navbar-links)
+(defmacro navbar+ (title navbar-links &body body)
   "Insert a standard navbar for bootstrap styling"
   (flet ((naventry (link &optional active)
            (when link
-            `(:li ,@(if active '(:class "active"))
-                  (:a :href ,(first link) ,(second link))))))
+             `(:li ,@(if active '(:class "active"))
+                   (:a :href ,(first link) ,(second link))))))
+    ;; todo handle case of no nav entries specially
     (let ((navbar-links (group navbar-links 2)))
-     `(htm
-       (:div :class "navbar navbar-inverse navbar-fixed-top" :role "navigation"
-             (:div :class "container"
-                   (:div :class "navbar-header"
-                         (:button :type "button" :class "navbar-toggle collapsed" :data-toggle "collapse" :data-target ".navbar-collapse"
-                                  (:span :class "sr-only" "toggle navigation")
-                                  #2=(:span :class "icon-bar") #2# #2#)
-                         (:a :class "navbar-brand" :href "#" (esc ,title)))
-                   (:div :class "navbar-collapse collapse"
-                         (:ul :class "nav navbar-nav"
-                              ,(naventry (first navbar-links) t)
-                              ,@(mapcar #'naventry (rest navbar-links))))))))))
+      `(htm
+        (:div :class "navbar navbar-inverse navbar-fixed-top" :role "navigation"
+              (:div :class "container"
+                    (:div :class "navbar-header"
+                          (:button :type "button" :class "navbar-toggle collapsed" :data-toggle "collapse" :data-target ".navbar-collapse"
+                                   (:span :class "sr-only" "toggle navigation")
+                                   #2=(:span :class "icon-bar") #2# #2#)
+                          (:a :class "navbar-brand" :href "#" (esc ,title)))
+                    (:div :class "navbar-collapse collapse"
+                          ,(when navbar-links
+                                 `(:ul :class "nav navbar-nav"
+                                       ,(naventry (first navbar-links) t)
+                                       ,@(mapcar #'naventry (rest navbar-links))))
+                          ,@body)))))))
+
+(defmacro navbar (title &rest navbar-links)
+  `(navbar+ ,title ,navbar-links))
+
+
+(defmacro bs-body (&body body)
+  `(htm (:div :class "container"
+              (:div :class "bs-body"
+                    ,@body))))
